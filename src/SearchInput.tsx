995d6fb5341c167470b40f0af66c9110cf2e21cb
kazuma1989 <kazuma.ebina@quipper.com>
2019-12-06 21:48:10 +0900

    refactor: Extract components

--- src/SearchInput.tsx
+++ src/SearchInput.tsx
@@ -8,44 +8,68 @@ export default function SearchInput() {
   const dispatch = useDispatch<Dispatch<Actions>>()
 
   return (
+    <Container>
+      <Title>Search posts</Title>
+
+      <SearchForm
+        text={query}
+        onChange={query =>
+          dispatch({
+            type: 'Search.Post.Input',
+            payload: {
+              query,
+            },
+          })
+        }
+        onSubmit={() =>
+          dispatch({
+            type: 'Search.Post.Submit',
+          })
+        }
+      />
+    </Container>
+  )
+}
+
+function Container({ children }: { children?: React.ReactNode }) {
+  return (
     <section className="section">
-      <div className="container">
-        <h1 className="title">Search posts</h1>
-
-        <form onClick={e => e.preventDefault()}>
-          <div className="field is-grouped">
-            <p className="control is-expanded">
-              <input
-                className="input"
-                type="search"
-                value={query}
-                onChange={e => {
-                  dispatch({
-                    type: 'Search.Post.Input',
-                    payload: {
-                      query: e.target.value,
-                    },
-                  })
-                }}
-              />
-            </p>
-
-            <p className="control">
-              <button
-                type="submit"
-                className="button is-info"
-                onClick={() => {
-                  dispatch({
-                    type: 'Search.Post.Submit',
-                  })
-                }}
-              >
-                Search
-              </button>
-            </p>
-          </div>
-        </form>
-      </div>
+      <div className="container">{children}</div>
     </section>
+  )
+}
+
+function Title({ children }: { children?: React.ReactNode }) {
+  return <h1 className="title">{children}</h1>
+}
+
+function SearchForm({
+  text,
+  onChange,
+  onSubmit,
+}: {
+  text?: string
+  onChange?(text: string): unknown
+  onSubmit?(): unknown
+}) {
+  return (
+    <form onClick={e => e.preventDefault()}>
+      <div className="field is-grouped">
+        <p className="control is-expanded">
+          <input
+            className="input"
+            type="search"
+            value={text}
+            onChange={e => onChange?.(e.target.value)}
+          />
+        </p>
+
+        <p className="control">
+          <button type="submit" className="button is-info" onClick={onSubmit}>
+            Search
+          </button>
+        </p>
+      </div>
+    </form>
   )
 }
--- src/SearchResults.tsx
+++ src/SearchResults.tsx
@@ -6,32 +6,58 @@ export default function SearchResults() {
   const posts = useSelector((state: RootState) => state.posts)
 
   return (
+    <Container>
+      <Title>Results</Title>
+
+      {posts.map(({ id, title, body }) => (
+        <MediaBox
+          key={id}
+          title={title}
+          body={body}
+          imgSrc="https://bulma.io/images/placeholders/128x128.png"
+        />
+      ))}
+    </Container>
+  )
+}
+
+function Container({ children }: { children?: React.ReactNode }) {
+  return (
     <section className="section">
-      <div className="container">
-        <h1 className="title">Results</h1>
-
-        {posts.map(({ id, title, body }) => (
-          <div key={id} className="box">
-            <article className="media">
-              <div className="media-left">
-                <figure className="image is-64x64">
-                  <img
-                    src="https://bulma.io/images/placeholders/128x128.png"
-                    alt="Image"
-                  />
-                </figure>
-              </div>
-
-              <div className="media-content">
-                <div className="content">
-                  <strong>{title}</strong>
-                  <p>{body}</p>
-                </div>
-              </div>
-            </article>
-          </div>
-        ))}
-      </div>
+      <div className="container">{children}</div>
     </section>
+  )
+}
+
+function Title({ children }: { children?: React.ReactNode }) {
+  return <h1 className="title">{children}</h1>
+}
+
+function MediaBox({
+  title,
+  body,
+  imgSrc,
+}: {
+  title?: string
+  body?: string
+  imgSrc?: string
+}) {
+  return (
+    <div className="box">
+      <article className="media">
+        <div className="media-left">
+          <figure className="image is-64x64">
+            <img src={imgSrc} alt={title} />
+          </figure>
+        </div>
+
+        <div className="media-content">
+          <div className="content">
+            <strong>{title}</strong>
+            <p>{body}</p>
+          </div>
+        </div>
+      </article>
+    </div>
   )
 }
