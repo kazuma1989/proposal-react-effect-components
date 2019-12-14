4e3c2a415d30fa5bf2375c771596da24651f0e52
kazuma1989 <kazuma.ebina@quipper.com>
2019-12-14 23:06:44 +0900

    refactor: Avoid confusing name

--- examples/3-code-splitting/index.tsx
+++ examples/3-code-splitting/index.tsx
@@ -2,18 +2,18 @@ import React, { Suspense, lazy } from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import baseReducer, { RootState } from './reducer'
+import reducer, { RootState } from './reducer'
 import { reducers } from './appendReducer'
 
 const SearchApp = lazy(() => import('./SearchApp'))
 
-reducers.push(baseReducer)
-const reducer = (state: RootState, action: any) =>
+reducers.push(reducer)
+const composedReducer = (state: RootState, action: any) =>
   reducers.reduce((s, r) => r(s, action), state)
 
 const preloadedState = undefined
 const storeEnhancer = (window as any).__REDUX_DEVTOOLS_EXTENSION__?.()
-const store = createStore(reducer, preloadedState, storeEnhancer)
+const store = createStore(composedReducer, preloadedState, storeEnhancer)
 
 ReactDOM.render(
   <Provider store={store}>
