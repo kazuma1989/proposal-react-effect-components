47c6779ecb0aa3a4820348934a785f27d7833651
kazuma1989 <kazuma.ebina@quipper.com>
2019-12-15 22:37:55 +0900

    chore: Remove first draft

--- examples/9-with-comments/util.ts
+++
@@ -1,6 +0,0 @@
-import { shallowEqual as shallowEqualOriginal } from 'react-redux'
-
-export const shallowEqual: <TSelected>(
-  left: TSelected,
-  right: TSelected,
-) => boolean = shallowEqualOriginal
